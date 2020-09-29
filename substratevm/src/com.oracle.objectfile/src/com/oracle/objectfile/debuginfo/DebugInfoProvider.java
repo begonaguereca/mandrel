2ca7a0118317ed202509423763b1bc6a70debada
Foivos Zakkak <fzakkak@redhat.com>
2020-09-30 02:33:50 +0300

    Chain debugLineInfo instances to get call-chain for nested inlining

--- substratevm/src/com.oracle.objectfile/src/com/oracle/objectfile/debuginfo/DebugInfoProvider.java
+++ substratevm/src/com.oracle.objectfile/src/com/oracle/objectfile/debuginfo/DebugInfoProvider.java
@@ -339,6 +339,11 @@ public interface DebugInfoProvider {
          * @return the line number for the outer or inlined segment.
          */
         int line();
+
+        /**
+         * @return the {@link DebugLineInfo} of the nested inline caller-line
+         */
+        DebugLineInfo getCaller();
     }
 
     interface DebugFrameSizeChange {
